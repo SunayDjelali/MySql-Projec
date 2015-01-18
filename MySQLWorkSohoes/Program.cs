006d3f274ec0d29b1fd5ebcc16a29b75c7ca4a2e
Az <quti@toshevo.net>
2015-01-18 16:57:50 +0200

    add

--- MySQLWorkSohoes/Program.cs
+++ MySQLWorkSohoes/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using MySQLShoese;
 
 namespace CSharpMySqlSample
 {
@@ -14,7 +15,9 @@ namespace CSharpMySqlSample
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMySql());
+            //Application.Run(new FormMySql());
+            Application.Run(new FormShoes());
+            
         }
     }
 }
